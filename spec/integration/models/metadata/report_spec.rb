f71073ccda18e3fb0d0776de8371f4207b4120e1
Jakub Mahnert <jakub.mahnert@gooddata.com>
2018-06-11 17:39:22 +0200

    internship talk reqs

--- spec/integration/models/metadata/report_spec.rb
+++ spec/integration/models/metadata/report_spec.rb
@@ -6,7 +6,7 @@
 
 require 'gooddata'
 
-describe GoodData::Report, :vcr, :constraint => 'slow' do
+describe GoodData::Report, :constraint => 'slow' do
   before(:all) do
     @client = ConnectionHelper.create_default_connection
     @project, @blueprint = ProjectHelper.load_full_project_implementation(@client)
@@ -49,6 +49,58 @@ describe GoodData::Report, :vcr, :constraint => 'slow' do
       @report.export_raw(@filename)
       expect(File).to exist(@filename)
       expect(File.read(@filename)).to eq("\"sum of Lines Changed\"\r\n\"11.00\"\r\n")
+    end
+  end
+
+  describe 'chart report' do
+    before do
+      blueprint = GoodData::Model::ProjectBlueprint.build('chart report prp') do |p|
+        p.add_date_dimension('committed_on')
+        p.add_dataset('devs') do |d|
+          d.add_anchor('attr.dev')
+          d.add_label('label.dev_id', :reference => 'attr.dev')
+          d.add_label('label.dev_email', :reference => 'attr.dev')
+        end
+        p.add_dataset('commits') do |d|
+          d.add_anchor('attr.commits_id')
+          d.add_fact('fact.lines_changed')
+          d.add_date('committed_on')
+          d.add_reference('devs')
+        end
+      end
+      project = GoodData::Project.create_from_blueprint(blueprint, auth_token: ConnectionHelper::GD_PROJECT_TOKEN, client: @client)
+
+      # Load data
+      commits_data = [
+        ['fact.lines_changed', 'committed_on', 'devs'],
+        [1, '01/01/2014', 1],
+        [3, '01/02/2014', 2],
+        [5, '05/02/2014', 3]]
+      project.upload(commits_data, blueprint, 'commits')
+
+      devs_data = [
+        ['label.dev_id', 'label.dev_email'],
+        [1, 'tomas@gooddata.com'],
+        [2, 'petr@gooddata.com'],
+        [3, 'jirka@gooddata.com']]
+      project.upload(devs_data, blueprint, 'devs')
+
+      # create a metric
+      @metric = project.facts('fact.lines_changed').create_metric
+      @metric.lock
+      @metric.save
+
+      @project = project
+    end
+    it 'can create one' do
+      label = @project.labels.to_a.find { |l| l.meta['identifier'] == 'label.dev_email' }
+
+      report = @project.create_report(title: 'Who did the most commits?', top: [@metric], left: ['label.dev_email'], format: 'chart', chart_part: { value_uri: label.uri })
+      expect(report).to be
+    end
+
+    after do
+      @project.delete
     end
   end
 end
